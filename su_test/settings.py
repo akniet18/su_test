c42530cc06d30a9f5aaa97c3d1e940172b38aa00
Akniet Kuanish <48211813+akniet18@users.noreply.github.com>
2023-11-16 15:11:29 +0600

    first

--- su_test/settings.py
+++ su_test/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'su_test.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'dffccb4d1CbfF6eEb1DeE*b66A42GD2A',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '28971',
     }
 }
 
