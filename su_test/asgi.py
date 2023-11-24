fbecea60402a5a6a1c7d5a526ff55f14cd244723
Akniet Kuanish <48211813+akniet18@users.noreply.github.com>
2023-11-24 21:51:47 +0600

    add etc

--- su_test/asgi.py
+++ su_test/asgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-import django
-# from django.core.asgi import get_asgi_application
-from channels.routing import get_default_application
+# import django
+from django.core.asgi import get_asgi_application
+# from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'su_test.settings')
-django.setup()
+# django.setup()
 
-application = get_default_application()
+application = get_asgi_application()
