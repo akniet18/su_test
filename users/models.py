0798202051bc6f6ca24a8cf3d8a4cff1fb1925a9
Akniet Kuanish <48211813+akniet18@users.noreply.github.com>
2023-12-26 16:57:35 +0600

    fixs

--- users/models.py
+++ users/models.py
@@ -26,6 +26,16 @@ class CustomManager(BaseUserManager):
             user.set_password(password)
             user.save(using=self._db)
             return user
+    
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(email, username, password, **extra_fields)
 
     def create_user(self, email, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
