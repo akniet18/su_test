96037d40a4d3b37019373162167fa311de9d87a6
Akniet Kuanish <48211813+akniet18@users.noreply.github.com>
2023-12-26 16:44:43 +0600

    fixs

--- users/models.py
+++ users/models.py
@@ -9,6 +9,27 @@ def user_photos_dir(instanse, filename):
     folder_name = f"{usrnme}/{datetime.today().strftime('%d_%m_%Y')}/{filename}"
     return folder_name
 
+def _create_user(self, email, username, password, **extra_fields):
+        values = [email, username]
+        field_value_map = dict(zip(self.model.REQUIRED_FIELDS, values))
+        for field_name, value in field_value_map.items():
+            if not value:
+                raise ValueError('The {} value must be set'.format(field_name))
+
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            username=username,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+def create_user(self, email, username, password=None, **extra_fields):
+    extra_fields.setdefault('is_staff', False)
+    extra_fields.setdefault('is_superuser', False)
+    return self._create_user(email, username,  password, **extra_fields)
 
 class User(AbstractUser):
     birth_date = models.DateField(null=True, blank=True)
