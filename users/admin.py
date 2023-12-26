0ffbda829621398ca3922c51cc626a49edf3adfd
Akniet Kuanish <48211813+akniet18@users.noreply.github.com>
2023-12-26 17:38:06 +0600

    fixs

--- users/admin.py
+++ users/admin.py
@@ -1,21 +1,22 @@
 from django import forms
 from django.contrib import admin
 from .models import User, HistoryModel
-from django.contrib.auth.forms import AdminPasswordChangeForm
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 
 class UserCreationForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        fields = ('email', 'username','password',"first_name", "last_name", 'is_staff', 'is_superuser',)
+        fields = ('email', 'username', 'first_name', 'last_name', 'card_id')
 
     def clean_password2(self):
         # Check that the two password entries match
-        password1 = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords don't match")
@@ -24,26 +25,49 @@ class UserCreationForm(forms.ModelForm):
     def save(self, commit=True):
         # Save the provided password in hashed format
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
+        user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
         return user
-    
+
+
 class UserChangeForm(forms.ModelForm):
     """A form for updating users. Includes all the fields on
     the user, but replaces the password field with admin's
-    disabled password hash display field.
+    password hash display field.
     """
     password = ReadOnlyPasswordHashField()
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'date_of_birth', 'is_active', 'is_admin')
+        fields = ('email', 'username', 'password',)
 
-class AuthorAdmin(BaseUserAdmin):
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
+    
+class AuthorAdmin(UserAdmin):
     add_form = UserCreationForm
     form = UserChangeForm
+
     list_display = ["username", "first_name", "last_name", "card_id"]
+
+    list_filter = ('is_staff',)
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal info', {'fields': ('card_id','first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'last_name', 'card_id', 'password1', 'password2')}
+        ),
+    )
 
 
 admin.site.register(User, AuthorAdmin)
